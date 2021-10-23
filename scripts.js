6387b9a0d6e16af1d2d36a384f5b7cbe47058f44
Jason Hunter <jahunter767@gmail.com>
2021-10-23 16:30:43 -0500

    preliminary page layout using css

--- scripts.js
+++ scripts.js
@@ -1,1 +1,9 @@
 /* Add your JavaScript to this file */
+
+// When a user suscribes, print:
+// "Thank you! Youjr email address <email> has been added to our mailing list!"
+// in the div with the class message
+//
+// If submitted with no email address output:
+// "Please enter a valid email address."
+// in the same area replacing any previous message
