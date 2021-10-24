5384d51d24fa2f4bea21f9599a40191fa66cda94
Jason Hunter <jahunter767@gmail.com>
2021-10-24 18:10:10 -0500

    added email validation js

--- scripts.js
+++ scripts.js
@@ -1,9 +1,26 @@
 /* Add your JavaScript to this file */
+"use strict";
 
-// When a user suscribes, print:
-// "Thank you! Youjr email address <email> has been added to our mailing list!"
-// in the div with the class message
-//
-// If submitted with no email address output:
-// "Please enter a valid email address."
-// in the same area replacing any previous message
+window.onload = function(){
+    let subBtn = document.getElementsByClassName("btn")[1];
+    let msg = document.getElementsByClassName("message")[0];
+
+    subBtn.addEventListener("click", (event) => {
+        event.preventDefault();
+
+        let alpha = "A-Za-z";
+        let alphaNum = `${alpha}0-9`;
+        let tld = `\\.[${alpha}][${alphaNum}]*`;
+        let hostname = `[${alpha}][${alphaNum}\\-]*[${alphaNum}]*`;
+        let username = `[${alpha}][${alphaNum}\\-\\.]*[${alphaNum}]*`;
+        let emailRegExp = new RegExp(`^${username}@${hostname}${tld}$`);
+        let email = document.getElementById("email").value.trim().match(emailRegExp);
+
+        if (email != null){
+            msg.textContent = `Thank you! Your email address ${email} ` +
+                `has been added to our mailing list!`;
+        }else{
+            msg.textContent = "Please enter a valid email address.";
+        } // End-if
+    }); // End-subBtn.eventListener
+}; // End-window.onload
